5b481a864c9f1c2960b417522f23d12be2d215c3
Philipp Winter <phw@brave.com>
2022-10-20 13:51:28 -0500

    Initial import.

---
+++ go.mod
@@ -0,0 +1,23 @@
+module github.com/brave-experiments/keysync-test
+
+go 1.19
+
+require github.com/brave/nitriding v1.1.3
+
+require (
+	github.com/brave/viproxy v0.1.2 // indirect
+	github.com/docker/libcontainer v2.2.1+incompatible // indirect
+	github.com/fxamacker/cbor/v2 v2.4.0 // indirect
+	github.com/go-chi/chi/v5 v5.0.7 // indirect
+	github.com/hf/nitrite v0.0.0-20211104000856-f9e0dcc73703 // indirect
+	github.com/hf/nsm v0.0.0-20211106132757-1ae65a6a69ae // indirect
+	github.com/mdlayher/socket v0.2.3 // indirect
+	github.com/mdlayher/vsock v1.1.1 // indirect
+	github.com/milosgajdos/tenus v0.0.3 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+	golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90 // indirect
+	golang.org/x/net v0.0.0-20220811182439-13a9a731de15 // indirect
+	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
+	golang.org/x/sys v0.0.0-20220825204002-c680a09ffe64 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+
+	"github.com/brave/nitriding"
+)
+
+// TODO: Set to 'false' on the other enclave.
+const originEnclave = true
+
+type keyMaterial struct {
+	Key string
+}
+
+func main() {
+	enclave := nitriding.NewEnclave(
+		&nitriding.Config{
+			FQDN:    "TODO: EC2-Host1",
+			Port:    8443,
+			UseACME: true,
+		},
+	)
+	go func() {
+		if err := enclave.Start(); err != nil {
+			log.Fatalf("Enclave terminated: %v", err)
+		}
+	}()
+
+	k := &keyMaterial{}
+
+	// We're the origin enclave.  Configure the key material.
+	if originEnclave {
+		k.Key = "foobar"
+		enclave.SetKeyMaterial(k)
+		log.Println("Set key material.  Waiting for other enclave to request it.")
+	} else {
+		if err := nitriding.RequestKeys("TODO: EC2-Host2", k); err != nil {
+			log.Fatalf("Failed to request keys from remote enclave: %v", err)
+		}
+		log.Printf("Successfully retrieved keys from remote enclave: %s", k.Key)
+	}
+
+	// Wait.
+	c := make(chan bool)
+	<-c
+}
